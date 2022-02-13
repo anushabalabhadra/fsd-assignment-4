54c52380e285bffe95154a7f41fed5023b906b92
anushabalabhadra <94507125+anushabalabhadra@users.noreply.github.com>
2022-02-13 13:06:00 +0530

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,26 @@
+import faker from "faker";
+const Content=(props)=>{
+   return(
+    <div class="ui cards">
+  
+  <div class="card">
+    <div class="content">
+    <img class="right floated mini ui image" src={faker.image.image()}/>
+      <div class="header">
+        Jenny Lawrence
+      </div>
+      <div class="meta">
+        New User
+      </div>
+      <div class="description">
+        Jenny requested permission to view your contact details
+      </div>
+    </div>
+    <div>
+      {props.children}
+    </div>
+  </div>
+</div>
+   )
+}
+export default Content;
